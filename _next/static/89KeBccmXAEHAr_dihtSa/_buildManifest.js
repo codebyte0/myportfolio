c2f5f09b1c727e097a2656d05ce636550f1de2ba
codebyte0 <codebyte.wd@gmail.com>
2024-09-03 17:25:56 -0700

    first commit

---
+++ _next/static/89KeBccmXAEHAr_dihtSa/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-2312390b9d603b03.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
